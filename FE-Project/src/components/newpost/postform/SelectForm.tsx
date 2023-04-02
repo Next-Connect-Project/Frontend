a4d82dcfe22c07f73186b8b483e68d1fc8abb11e
hyeJune <chj950807@naver.com>
2023-04-03 03:36:42 +0900

    FEAT: NEWPost UI/UX 구현

---
+++ FE-Project/src/components/newpost/postform/SelectForm.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useRef, useEffect } from "react";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { Option, SelectBoxProps } from "../Post.intefact";
+
+export default function SelectForm(props: SelectBoxProps) {
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+  const target: any = useRef("");
+
+  const outsideSelect = (e: any) => {
+    if (target.current != null && !target.current.contains(e.target)) {
+      setShowOptions(false);
+    }
+  };
+  const ShowSelectOptions = () => {
+    setShowOptions(true);
+  };
+
+  const ClickOptions = (value: string) => {
+    setShowOptions(false);
+    props.setData(value);
+  };
+  useEffect(() => {
+    document.addEventListener("mousedown", outsideSelect);
+  });
+  return (
+    <div ref={target}>
+      <ul className="select_form" onClick={ShowSelectOptions}>
+        <li className="selected_option">{props.data}</li>
+        <div className="select_right">
+          <div className="division">&nbsp;</div>
+          <span>
+            <MdKeyboardArrowDown className="picker" />
+          </span>
+        </div>
+      </ul>
+      {showOptions ? (
+        <ul className="select_options">
+          {props.options.map((option: Option) => {
+            return (
+              <li
+                key={option.value}
+                className="select_option"
+                onClick={(e) => ClickOptions(option.value)}
+              >
+                {option.value}
+              </li>
+            );
+          })}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
