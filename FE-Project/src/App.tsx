e4515a7c25f69b6dd3bc24d86653113f0098b220
Song ji su <thdwltn0727@gmail.com>
2023-04-12 01:21:54 +0900

    FEAT: Project페이지로 Route, Link 추가
    - App.tsx : 페이지 신규 생성으로 Route 추가했습니다.
    - MainCard.tsx :  Link 속성 추가했습니다.
    ㄴ home.scss : Link 속성추가로 인해 생기는 파란줄 제거를 위해 position 속성 변경했습니다.

--- FE-Project/src/App.tsx
+++ FE-Project/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import ProjectDetail from "./components/project/ProjectDetail";
 import Home from "./page/Home"
 import Login from "./page/Login";
 import NewPost from "./page/NewPost";
 import NotFound from "./page/NotFound";
+import Project  from "./page/Project";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/post" element={<NewPost />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/project/:id" element={<Project />}></Route>
         <Route path={"*"} element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
