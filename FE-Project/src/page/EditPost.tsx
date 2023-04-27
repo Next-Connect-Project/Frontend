d494d4059fde902dc6694e19ab3ca0551d73967e
Song ji su <thdwltn0727@gmail.com>
2023-04-27 16:09:39 +0900

    [FEAT] 수정페이지 뼈대 작성
    - 미구현,,😔

---
+++ FE-Project/src/page/EditPost.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/header/Header'
+import Edit from '../components/newpost/Edit'
+import Footer from '../components/footer/Footer'
+
+export default function EditPost() {
+  return (
+      <>
+          <Header />
+          <Edit />
+          <Footer />
+      </>
+  )
+}
