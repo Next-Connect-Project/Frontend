118e34b9dcad8173b47e6bb46ab6883a2dd2748d
hyeJune <chj950807@naver.com>
2023-06-01 13:56:57 +0900

    홍보글 API 연동

--- FE-Project/src/hooks/axios/Promotion.ts
+++ FE-Project/src/hooks/axios/Promotion.ts
@@ -10,14 +10,13 @@ export const getPromotionCard = async (
     .get(url, {
       params: { page: page, limit: limit, sorting: sorting },
     })
-      .then((res) => {
+    .then((res) => {
       return res.data.response;
     })
     .catch((e) => {
       console.log(e);
     });
 };
-
 
 export const getPromotionData = async (
   id: string | undefined,
@@ -28,6 +27,36 @@ export const getPromotionData = async (
     .get(url, { headers: { Authorization: `Bearer ${token}` } })
     .then((res) => {
       return res.data.response;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+export const ChangeLikeStatus = async (
+  id: string | undefined,
+  token: string | null
+) => {
+  const url = `http://ec2-13-209-222-139.ap-northeast-2.compute.amazonaws.com:8080/api/promotion/like/${id}`;
+  return await axios
+    .post(url, {}, { headers: { Authorization: `Bearer ${token}` } })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+export const DeletePromotionPost = async (
+  id: string | undefined,
+  token: string | null
+) => {
+  const url = `http://ec2-13-209-222-139.ap-northeast-2.compute.amazonaws.com:8080/api/promotion/delete/${id}`;
+  return await axios
+    .delete(url, { headers: { Authorization: `Bearer ${token}` } })
+    .then((res) => {
+      return res.data;
     })
     .catch((e) => {
       console.log(e);
