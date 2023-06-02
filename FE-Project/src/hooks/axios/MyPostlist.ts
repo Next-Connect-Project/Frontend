13172b34a8c596873c7eb1e5fe4504587d1651f3
Song ji su <thdwltn0727@gmail.com>
2023-06-03 02:19:44 +0900

    FEAT: 내가 쓴 글 API 연동 및 UI 구현

--- FE-Project/src/hooks/axios/MyPostlist.ts
+++ FE-Project/src/hooks/axios/MyPostlist.ts
@@ -9,9 +9,27 @@ export const getMyPostlist = async (
       { headers: { Authorization: token, }
     })
     .then((res) => {
+      console.log(res);
+      return res.data.response;      
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+// 수정 필요
+export const getMyPromotion = async (
+  token: string | null
+) => {
+  const url = `http://ec2-13-209-222-139.ap-northeast-2.compute.amazonaws.com:8080/api/`;
+  return await axios
+    .get(url, { headers: { Authorization: token } })
+    .then((res) => {
       return res.data.response;
     })
     .catch((e) => {
       console.log(e);
     });
 };
+
+
