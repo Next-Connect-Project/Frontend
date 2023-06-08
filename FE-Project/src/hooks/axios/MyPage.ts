808f467a0b9580e515600cfbf06b35809fb3c19b
Song ji su <thdwltn0727@gmail.com>
2023-06-08 18:40:08 +0900

    REFACTOR: api 주소 변경

--- FE-Project/src/hooks/axios/MyPage.ts
+++ FE-Project/src/hooks/axios/MyPage.ts
@@ -16,7 +16,7 @@ export const getMyRecuritment = async (token: string | null) => {
 
 // 수정 필요
 export const getMyPromotion = async (token: string | null) => {
-  const url = `http://www.nextconnect.shop/api/`;
+  const url = `http://www.nextconnect.shop/api/promotion/my`;
   return await axios
     .get(url, { headers: { Authorization: token } })
     .then((res) => {
