57a34efdf00eed920350d48e5839e978bfabf826
Song ji su <thdwltn0727@gmail.com>
2023-06-08 18:42:25 +0900

    FEAT: 홍보 api 활용 코드 작성

--- FE-Project/src/components/mypost/MyPostMain.tsx
+++ FE-Project/src/components/mypost/MyPostMain.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { AiFillFolderOpen } from "react-icons/ai";
 import { useAppSelector } from "../../hooks/redux/store";
-import { Mypostprops } from "../mypost/MyPost.interface";
+import { MyRecuritment, MyPromotion } from "../mypost/MyPost.interface";
 import { getMyRecuritment, getMyPromotion } from "../../hooks/axios/MyPage";
-import { PromotionDetail } from "../promotiondetail/PromotionDetail.interface";
 import { Link , useParams} from "react-router-dom";
 
 export default function MyPostMain() {
-  const [list, setList] = useState<Mypostprops[]>([]);
-  const [promotionlist, setPromotionlist] = useState<PromotionDetail[]>([]);
+  const [list, setList] = useState<MyRecuritment[]>([]);
+  const [promotionlist, setPromotionlist] = useState<MyPromotion[]>([]);
   const token = useAppSelector((state) => state.login);
   const { id } = useParams();
 
@@ -19,16 +18,18 @@ export default function MyPostMain() {
     setList (data.recruitments);
   };
 
-  // const getPromotion = async () => {
-  //   const  = await getMyPromotion(token.token);
-  //   setPromotionlist();
-  // };
+  const getPromotion = async () => {
+    const data = await getMyPromotion(token.token);
+    console.log("promotion");
+    console.log (data);
+    setPromotionlist(data.promotionOverviewDtoList);
+  };
 
 
 
   useEffect(() => {
     getData();
-    // getPromotion();
+    getPromotion();
   }, []);
 
   return (
@@ -41,6 +42,7 @@ export default function MyPostMain() {
       <div className="announcement">
         모집
       </div>
+
       {list.map((item) => (
         <div className="post_wrapper" key={item.id}>
           <div className="content"><span>제목:</span> {item.title}</div>
@@ -59,15 +61,23 @@ export default function MyPostMain() {
       </div>
       
       
-      <div className="post_wrapper">
-        <div className="content"><span>제목:</span> </div>
-        <div className="content"><span>작성일:</span> </div>
-        <div className="content"><span>모집 마감:</span> </div>
-        <div className="content"><span>마감 여부:</span> </div>
-        <Link className="link" to={`/promotion`}>
+        {promotionlist.map( (item) => (
+        <div className="post_wrapper">
+          <div className="content"><span>제목:</span> {item.title} </div>
+          <div className="content"><span>작성일:</span> {item.createdAt}</div>
+          <div className="content"><span>모집 마감:</span> {item.abstractContent}</div>
+          <div className="content"><span>추천수:</span> {item.likeCount}</div>
+          <div className="content"><span>조회수:</span> {item.view}</div>
+          
+          <Link className="link" to={`/promotion/${item.id}`}>
             <div className="owner_button">상세보기</div>
-        </Link>
-      </div>
+          </Link>
+        </div>
+        ))}
+
+
+
+
     </div>
   );
 }
