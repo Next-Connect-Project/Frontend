72824df8a304171842d697ea8d466d695cfb4c59
Song ji su <thdwltn0727@gmail.com>
2023-06-08 18:42:03 +0900

    REFACTOR: 홍보 데이터 타입 선언

--- FE-Project/src/components/mypost/MyPost.interface.tsx
+++ FE-Project/src/components/mypost/MyPost.interface.tsx
@@ -1,4 +1,4 @@
-export interface Mypostprops {
+export interface MyRecuritment {
   id: number;
   category: String; // PROJECT, STUDY
   memberName: String; // 작성자 이름
@@ -8,6 +8,20 @@ export interface Mypostprops {
   tech: []; // 기술 스택
   state : String; // 마감 여부 CLOSE, OPEN 
 }
+
+export interface MyPromotion {
+  id: number;               	//게시물 번호
+  createdAt: string;    //게시물 생성 날짜
+  title: String;          //게시물 제목
+  content: String;        //게시물 내용
+  name: String;            //게시물 작성자
+  abstractContent: String;  //게시물 요약내용
+  likeCount: number;          //게시물 추천수
+  likeStatus: boolean;     //게시물 추천상태
+  view: number;               //게시물 조회수
+}
+
+
 
 export interface MyInfo {
   email: string;
